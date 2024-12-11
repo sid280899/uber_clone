192dba678b2819d1653e1c15fa017f8080a2622b
siddhesh <siddheshneharkar70@gmail.com>
2024-12-11 16:42:50 +0530

    user profile route  create

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,15 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 
+connectToDb();
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-connectToDb();
 
 app.get('/', (req, res) => {
   res.send('Hello world');
